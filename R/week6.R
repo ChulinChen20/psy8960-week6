aa5fbe69058f034afa910931a3416279ba73b1d0
“Chulin <chen6496@umn.edu>
2023-02-22 05:07:08 -0600

    final commit

--- R/week6.R
+++ R/week6.R
@@ -13,11 +13,14 @@ mean(str_length(citations_txt))
 
 # Data Cleaning
 sample(citations_txt,size=10)
+
 citations_tbl <- tibble(line=c(1:length(citations_txt)),cite=citations_txt) %>%
   mutate(cite=str_remove_all(cite,"[\"\']")) %>%
   mutate(year=str_match(cite,"\\((\\d{4})")[,2]) %>%
   mutate(page_start=str_match(cite,"(\\d+)-\\d+\\.")[,2]) %>%
   mutate(perf_ref=str_detect(cite,"performance|Performance")) %>%
   mutate(title=str_match(cite,"\\(\\d{4}(,\\s[\\w+\\s-]+)?\\)\\.\\s([^\\.\\(\\)]+(\\s\\(\\w+\\sed\\.\\))?[\\.\\?\\!])\\s")[,3]) %>%
-  mutate(first_author=str_match(cite,"^[A-Za-z-]+,[\\sA-Z\\.]+")) %>%
-  filter(!is.na(first_author))
+  mutate(first_author=str_match(cite,"^[A-Za-z-]+,[\\sA-Z\\.]+"))
+
+  sum(!is.na(citations_tbl$first_author))
+  
