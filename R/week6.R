7883105717b0776028142a92d73abcdddba931b3
ChulinChen20 <69262697+ChulinChen20@users.noreply.github.com>
2023-02-27 01:20:48 -0600

    Update week6.R

--- R/week6.R
+++ R/week6.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 
 # Data Import
-citations <- stri_read_lines("../data/citations.txt",encoding="Latin-1")
+citations <- stri_read_lines("../data/citations.txt",encoding="WINDOWS-1252")
 citations_txt <-citations[citations!=""]
 length(citations)-length(citations_txt)
 mean(str_length(citations_txt))
@@ -16,9 +16,9 @@ sample(citations_txt,size=10)
 
 citations_tbl <- tibble(line=c(1:length(citations_txt)),cite=citations_txt) %>%
   mutate(cite=str_remove_all(cite,"[\"\']")) %>%
-  mutate(year=str_match(cite,"\\((\\d{4})")[,2]) %>%
+  mutate(year=str_match(cite,"\\((\\d{4})(,\\s[\\w+\\s-]+)?\\)")[,2]) %>%
   mutate(page_start=str_match(cite,"(\\d+)-\\d+\\.")[,2]) %>%
-  mutate(perf_ref=str_detect(cite,"performance|Performance")) %>%
+  mutate(perf_ref=str_detect(str_to_lower(cite),"performance")) %>%
   mutate(title=str_match(cite,"\\(\\d{4}(,\\s[\\w+\\s-]+)?\\)\\.\\s([^\\.\\(\\)]+(\\s\\(\\w+\\sed\\.\\))?[\\.\\?\\!])\\s")[,3]) %>%
   mutate(first_author=str_match(cite,"^[A-Za-z-]+,[\\sA-Z\\.]+"))
 
